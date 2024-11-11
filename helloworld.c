ff9d858be1c15b26c33242379cc67332dca99178
caokai <1103661925@qq.com>
2024-11-11 15:52:49 +0800

    test

---
+++ helloworld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    const double pi = 3.1415;
+    double r;
+    double y;//圆的周长
+    double x;//
+
+    printf("how old are you?\n");
+    scanf("%d",&x);
+
+    printf("you are %d years old",x);
+
+    return 0;
+
+}
